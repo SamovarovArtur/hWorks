46913860680388cf7f7c9e4dea3ba343ee1ea8f1
art <samovarovartur@gmail.com>
2018-05-01 14:18:40 +0300

    Second commit

--- app6.js
+++ app6.js
@@ -1,9 +1,29 @@
 var array = ['when','new', 'legend', 'not', 'born',];
-function elementLenght(word) {
-    return word.length === 3
+function elementLenght(word){
+    return word.length === 3;
 }
 console.log(array.some(elementLenght))
+/////////////////////////////////////////////
 function includeLetter(letter){
     return letter.includes('n')
 }
 console.log(array.every(includeLetter))
+//////////////////////////////////////////////
+array.forEach(function (item,i,array) {
+    item = item + " " + i;
+    console.log(item)
+})
+//////////////////////////////////////////////
+var newArray = array.filter(function (newWords) {
+    return newWords.length === 3
+})
+console.log(newArray)
+//////////////////////////////////////////////
+var newArray2 = array.reduce(function (prevValue, nextValue,index,array) {
+    return prevValue + ' ' + nextValue
+})
+console.log(newArray2)
+//////////////////////////////////////////////
+array.map(function (digits) {
+    console.log(digits.number)
+})
